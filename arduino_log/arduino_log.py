9e16ca1f0a0b4a660bc91864bea96efd6e97b196
K. C. Juntunen <kcjuntunen@frontier.com>
2016-03-29 16:05:43 -0400

    I'm not clever enough to test these changes yet

--- arduino_log/arduino_log.py
+++ arduino_log/arduino_log.py
@@ -10,183 +10,134 @@ class arduino_log():
         """Variable-ize everything in the thingspeak config file."""
         with open(config_file) as data_file:
             self.config_data = json.load(data_file)
-            self.local_db = self.config_data["localDB"]
-            self.key = self.config_data["key"]
-            self.api_key = self.config_data["api_key"]
-            self.unit = self.config_data["unit"]
-            self.smtp_server = self.config_data["smtp_server"]
-            self.sender = self.config_data["sender"]
-            self.recipients = self.config_data["recipients"]
-            self.threshold = self.config_data["light_threshold"]
-            self.day_start = self.config_data["day_start"]
-            self.day_end = self.config_data["day_end"]
-            self.alerts = self.config_data["alerts"]
-            self.ser = serial.Serial(self.config_data["serial_port"],
-                                     self.config_data["baud"])
+        self.local_db = self.config_data["localDB"]
+        self.key = self.config_data["key"]
+        self.api_key = self.config_data["api_key"]
+        self.unit = self.config_data["unit"]
+        self.smtp_server = self.config_data["smtp_server"]
+        self.sender = self.config_data["sender"]
+        self.recipients = self.config_data["recipients"]
+        #self.threshold = self.config_data["light_threshold"]
+        self.day_start = self.config_data["day_start"]
+        self.day_end = self.config_data["day_end"]
+        self.alerts = self.config_data["alerts"]
+        self.thspfreq = self.config_data["thingspeak_freq"]
+        self.ser = serial.Serial(self.config_data["serial_port"],
+                                 self.config_data["baud"])
         self.sqlw = sqli.sqlite_writer(self.local_db,
                                        self.config_data["labels"])
         self.thingspeak = thsp.ThingspeakInterface(config_file)
-        self.sent_on = True
-        self.sent_off = False
+        self.sent = {}
+        for i in self.config_data["alerts"]:
+            self.sent[u.unicode_to_string(i)] = False
+        #self.sent_on = True
+        #self.sent_off = False
         self.labels = self.config_data["labels"]
 
     def ok_to_send(self):
         return u.ok_to_send(self.day_start, self.day_end)
 
     def send_email(self, subj, msg):
-        try:
-            sender = self.sender
-            receivers = self.recipients
-            message = "From: " + self.unit  + " <no_real_email@nobody.com>"
-            message += "\nTo: "
+        if len(self.sender) > 0 or len(self.recipients) < 1:
+            try:
+                sender = self.sender
+                receivers = self.recipients
+                message = "From: " + self.unit  + " <no_real_email@nobody.com>"
+                message += "\nTo: "
 
-            rec_cnt = len(receivers)
-            cnt = 1
-            for s in receivers:
-                message += "<" + s
-                message += ">"
-                if cnt < rec_cnt:
-                    message += ", "
+                rec_cnt = len(receivers)
+                cnt = 1
+                for s in receivers:
+                    message += "<" + s
+                    message += ">"
+                    if cnt < rec_cnt:
+                        message += ", "
 
-            message += "\nSubject: "
+                    message += "\nSubject: "
 
-            message += subj + "\n\n"
-            message += msg + "\n"
-            smtpo = smtplib.SMTP(self.smtp_server)
-            smtpo.sendmail(sender, receivers, message)
-            return True
-        except Exception, e:
-            self.sqlw.insert_alert("Failure sending email: {0}"
-                                 .format(e), 0, 0, 0)
+                    message += subj + "\n\n"
+                    message += msg + "\n"
+                    smtpo = smtplib.SMTP(self.smtp_server)
+                    smtpo.sendmail(sender, receivers, message)
+                    return True
+            except Exception, e:
+                self.sqlw.insert_alert("Failure sending email: {0}"
+                                       .format(e), 0, 0, 0)
+                return False
+        else:
             return False
 
-    def send_data2(self):
-        try:
-            l = self.ser.readline()
-            line = l.split(":")
-        except:
-            msg = "Couldn't split string: "
-            if l:
-                msg += l
-            self.sqlw.insert_alert(msg, 0, 0, 0);
-
-        try:
-            self.sqlw.insert_ser_line(l)
-        except:
-            print "Couldn't insert data."
-
-        paramsd = {'field1': line[1],
-                   'field2': line[2],
-                   'field3': line[3],
-                   'field4': line[4],
-                   'field5': line[5],
-                   'field6': line[6],
-                   'field7': os.getloadavg()[0],
-                   'field8': line[7],
-                   'key': self.key}
-
-        if float(line[4]) < self.threshold:
-            if not self.sent_off:
-                today = datetime.datetime.now()
-                paramsd['status'] = "Lights are out."
-                msg = "The lights are out in {0}. ({1} UTC)".format(
-                    self.unit, today.strftime('%b %d %Y %H:%M'))
-                t = 0
-                if self.thingspeak.tweet(msg):
-                    t = 1
-                e = 0
-                if self.ok_to_send() and self.send_email(paramsd['status'], msg):
-                    e = 1
-
-                self.sqlw.insert_alert("Lights are out.", e, t, 1)
-                self.sent_off = True
-                self.sent_on = False
-
-        if float(line[4]) > self.threshold:
-            if not self.sent_on:
-                today = datetime.datetime.now()
-                paramsd['status'] = "Lights are back on."
-                msg = "The lights are back on in {0}. ({1} UTC)".format(
-                    self.unit, today.strftime('%b %d %Y %H:%M'))
-                t = 0
-                if self.thingspeak.tweet(msg):
-                    t = 1
-                e = 0
-                if self.ok_to_send() and self.send_email(paramsd['status'], msg):
-                    e = 1
-
-                self.sqlw.insert_alert("Lights are back on.", 0, t, 1)
-                self.sent_on = True
-                self.sent_off = False
-
-        params = urllib.urlencode(paramsd)
-
-        headers = {"Content-type": "application/x-www-form-urlencoded",
-                   "Accept": "text/plain"}
-
-        conn = httplib.HTTPConnection("api.thingspeak.com:80")
-
-        try:
-            conn.request("POST", "/update", params, headers)
-            response = conn.getresponse()
-            #print response.status, response.reason
-            data = response.read()
-            conn.close()
-        except:
-            self.sqlw.insert_alert("Connection failed", 0, 0, 0)
-
-    def process_data(self, json_ob):
-        # Instead of millis:presence:lrs:srs:ll:hum:temp:lt:pir,
-        # how about
-        # {
-        #     "millis()": 4373838,        
-        #     "distance": 600,
-        #     "Switch1": 0,
-        #     "Switch2": 0,
-        #     "LightLevel": 300,
-        #     "Humidity": 28,
-        #     "Temperature": 72,
-        #     "Pressure": 1011,            
-        # }
-        data = json.load(json_ob)
-
-    def log_data(self, ser):
-        while True:
-            line = ser.readline()
-            data = self.decode_string(line)
-            self.check_alerts(data)
+    def log_data(self):
+        line = self.ser.readline()
+        data = self.decode_string(line, 0)
+        self.check_alerts(data)
+        if len(data) > 0:
             self.sqlw.insert_dict(data)
 
     def check_alerts(self, datadict):
         for a in self.alerts:
-            if datadict[a] > abs(self.config_data[a]):
-                if self.ok_to_send():
-                    self.send_email(self.unit, a + " exceeded.")
-                    self.thingspeak.tweet(a + " exceeded in " + self.unit)
+            try:
+                b = u.unicode_to_string(a)
+                if abs(datadict [b]) > abs(self.config_data[b]):
+                    if self.ok_to_send() and not self.sent[b]:
+                        message = b + " exceeded in " + self.unit
+                        esent = 0
+                        if self.send_email(self.unit, message):
+                            esent = 1
+                        tsent = 0
+                        if self.thingspeak.tweet(message):
+                            tsent = 1
+                        self.sqlw.insert_alert(message, esent, tsent, 0)
+                        self.sent[b] = True
+            except Exception as e:
+                print "Exception: {0}\n".format(e)
 
-    def decode_string(self, line):
+    def check_return(self, datadict):
+        for a in self.alerts:
+            try:
+                b = u.unicode_to_string(a)
+                if abs(datadict [b]) < abs(self.config_data[b]):
+                    if self.ok_to_send() and not self.sent[b]:
+                        message = b + " returned to normal in " + self.unit
+                        esent = 0
+                        if self.send_email(self.unit, message):
+                            esent = 1
+                        tsent = 0
+                        if self.thingspeak.tweet(message):
+                            tsent = 1
+                        self.sqlw.insert_alert(message, esent, tsent, 0)
+                        self.sent[b] = False
+            except Exception as e:
+                print "Exception: {0}\n".format(e)
+
+    def decode_string(self, line, skip):
         data = {}
         line_array = line.split(":")
-        if not len(line_array) == len(self.labels) + 1:
+        if not len(line_array) == len(self.labels) + abs(skip):
             return data
-        count = -1
+        count = abs(skip) * -1
         for field in line_array:
             if count > -1:
-                data[u.unicode_to_string(self.labels[count])] = float(field)
+                try:
+                    data[u.unicode_to_string(self.labels[count])] = float(field)
+                except:
+                    return data
             count += 1
         return data
 
     def loop(self):
         while True:
             try:
-                self.send_data2()
+                self.log_data()
             except Exception as e:
-                self.sqlw.insert_alert("Exception: {0}".format(e), 0, 0, 0)
+                print("Exception: {0}\n".format(e.message))
+                exit(1)
+                #self.sqlw.insert_alert("Exc3ption: {0}".format(e.message), 0, 0, 0)
 
 def start():    
     import ip as ip
     ip.broadcast_ip()
-    moni = arduino_log('/etc/ardino_log.json')
+    moni = arduino_log('/etc/arduino_log.json')
     moni.loop()
 
 
