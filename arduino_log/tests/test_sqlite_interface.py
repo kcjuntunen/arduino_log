7e3de49b28331d9b21022fa894917e0668373a3e
K. C. Juntunen <kcjuntunen@frontier.com>
2016-03-29 10:04:38 -0400

    update tests

--- arduino_log/tests/test_sqlite_interface.py
+++ arduino_log/tests/test_sqlite_interface.py
@@ -1,26 +1,45 @@
 from unittest import TestCase
 import random
 from arduino_log import sqlite_interface as sqi
-from arduino_log import thingspeak
-from arduino_log import arduino_log
+#from arduino_log import thingspeak
+#from arduino_log import arduino_log
 
 SAMPLE_JSON = '{"a": 5847, "b": -42, "c": 482}'
+fields = ['a', 'b', 'c']
 
 class TestCreate(TestCase):
     def setUp(self):        
-        self.s = sqi.sqlite_writer("/tmp/test.db", SAMPLE_JSON)
-        self.q = sqi.sqlite_reader("/tmp/test.db", SAMPLE_JSON)
+        self.s = sqi.sqlite_writer("/tmp/test.db", fields)
+        self.s.insert_data(SAMPLE_JSON)
+        random.seed()
+        x = random.randrange(1, 500000) / 1000.0
+        self.s.insert_data('{"a": 5847, "b": ' + str(x)  + ', "c": 482}')
+        self.q = sqi.sqlite_reader("/tmp/test.db", fields)
         
     def test_sqlite_create(self):
         self.s.print_tables()
 
     def test_insert_data(self):
-        random.seed()
+        x = 0
+        for i in range(500):
+            v = random.randrange(1, 500000) / 1000.0
+            w = random.randrange(1, 500000) / 1000.0
+            x = random.randrange(1, 500000) / 1000.0
+            self.s.insert_data('{"a": ' + str(v) + ', "b": ' + str(x)  + ', "c": ' + str(w) + '}')
+            
         x = random.randrange(1, 500000) / 1000.0
-        self.s.insert_data('{"a": 5847, "b": ' + str(x)  + ', "c": 482}')
-        self.assertEqual(self.q.get_last_record()[0][2], x)
+        self.s.insert_data('{"a": 657, "b": ' + str(x)  + ', "c": 754}')
+
+        y = self.q.get_last_record()[0]
+        for i in y:
+            print(i)
+        self.assertEqual(y[1], x)
 
     def test_read_data(self):
         lr = self.q.get_last_record()[0]
         self.assertEqual(lr[0], 5847)
-        self.assertEqual(lr[1], 482)
+        self.assertEqual(lr[2], 482)
+
+    def tearDown(self):
+        import os
+        os.remove(self.s.db_filename)
--- arduino_log/tests/test_thingspeak.py
+++ arduino_log/tests/test_thingspeak.py
@@ -5,18 +5,24 @@ from arduino_log import thingspeak
 from arduino_log import arduino_log
 
 SAMPLE_JSON = '{"a": 5847, "b": -42, "c": 482}'
+fields = ['a', 'b', 'c']
 
 class TestCreate(TestCase):
     def setUp(self):        
-        self.s = sqi.sqlite_writer("/tmp/test.db", SAMPLE_JSON)
-        self.q = sqi.sqlite_reader("/tmp/test.db", SAMPLE_JSON)
+        self.s = sqi.sqlite_writer("/tmp/test.db", fields)
+        self.s.insert_data(SAMPLE_JSON)
+        self.q = sqi.sqlite_reader("/tmp/test.db", fields)
         
     def test_create_url(self):
-        t = thingspeak.ThingspeakInterface('/home/juntunenkc/git/arduino_log/etc/arduino_log.json', SAMPLE_JSON)
+        t = thingspeak.ThingspeakInterface('/home/juntunenkc/git/arduino_log/etc/arduino_log.json')
         u = t.create_url(self.q.get_last_record_dict())
         print(u)
         self.assertGreater(len(u), 1)
 
     def test_send_data(self):
-        t = thingspeak.ThingspeakInterface('/home/juntunenkc/git/arduino_log/etc/arduino_log.json', SAMPLE_JSON)
+        t = thingspeak.ThingspeakInterface('/home/juntunenkc/git/arduino_log/etc/arduino_log.json')
         t.send_data()
+
+    def tearDown(self):
+        import os
+        os.remove(self.s.db_filename)
