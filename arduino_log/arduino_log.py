9dd22db40dc95d082b07f3b3f5d418cb5d7181b9
K. C. Juntunen <kcjuntunen@frontier.com>
2016-04-29 08:39:18 -0400

    format comments

--- arduino_log/arduino_log.py
+++ arduino_log/arduino_log.py
@@ -14,9 +14,9 @@ For processing Arduino output.
     """
     def __init__(self, config_file):
         """
-Variable-ize everything in the thingspeak config file.
-TODO: Put this stuff all in an update function. That way, perhaps,
-the variables could be updated while arduino-log is running.
+Variable-ize everything in the thingspeak config file. TODO: Put this
+stuff all in an update function. That way, perhaps, the variables
+could be updated while arduino-log is running.
         """
         with open(config_file) as data_file:
             self.config_data = json.load(data_file)
@@ -51,7 +51,7 @@ the variables could be updated while arduino-log is running.
         self.sentinlastfive = not self.sentinlastfive
 
     def poll(self):
-        if self.logfreq > 1:            
+        if self.logfreq > 1:
             self.ser.write(ARDUINO_POLL)
             Timer(self.logfreq, self.poll, ()).start()
         else:
@@ -62,8 +62,8 @@ the variables could be updated while arduino-log is running.
 
     def send_email(self, subj, msg):
         """
-Sends an email. subj, and msg are self-explanatory.
-TODO: Make it clever enough to support passwords.
+Sends an email. subj, and msg are self-explanatory. TODO: Make it
+clever enough to support passwords.
         """
         if (len(self.sender) > 0 or len(self.recipients) < 1):
             try:
@@ -97,9 +97,10 @@ TODO: Make it clever enough to support passwords.
     def process_line(self, line):
         """
 When a line is received, process it conditionally.
-{ "Poll": { dict here } } -> record to snapshot log
-{ "RFID": { dict here } } -> Drop RFID info somewhere useful
-{ "Event": { dict here } } ->  Log to event db
+
+        { "Poll": { dict here } } -> record to snapshot log
+        { "RFID": { dict here } } -> Drop RFID info somewhere useful
+        { "Event": { dict here } } ->  Log to event db
         """
         try:
             json_ob = json.loads(line)
@@ -124,9 +125,9 @@ the JSON string can't have newlines inside it.
 
     def log_snapshot(self, data):
         """
-Check if certain thresholds have been overrun.
-Send a proper dictionary to the sql interface class.
-TODO: DB should be optional. Merely sending alerts would be useful.
+Check if certain thresholds have been overrun.  Send a proper
+dictionary to the sql interface class.  TODO: DB should be
+optional. Merely sending alerts would be useful.
         """
         if data is not None and len(data) > 0:
             self.check_alerts(data)
@@ -134,27 +135,22 @@ TODO: DB should be optional. Merely sending alerts would be useful.
 
     def process_rfid(self, data):
         """
-Drop this data somewhere it'll be visible to apache/httpd and php. This will probably be unusable without a local server of some sort.
+Drop this data somewhere it'll be visible to apache/httpd and php.
+This will probably be unusable without a local server of some sort.
         """
         pass
 
     def process_event(self, data):
         """
-Cleverly store event data in a db. I can't imagine a way to handle this without a definition in the config file.
+Cleverly store event data in a db. I can't imagine a way to handle
+this without a definition in the config file.
         """
         pass
 
-    # def log_data2(self):
-    #     line = self.ser.readline()
-    #     data = self.decode_string2(line, 0)
-    #     self.check_alerts(data)
-    #     if data is not None and len(data) > 0:
-    #         self.sqlw.insert_dict(data)
-
     def check_alerts(self, datadict):
         """
-Here, a dictionary is examined accoring to thresholds set in the config
-file.
+Here, a dictionary is examined accoring to thresholds set in the
+config file.
         """
         for alert in self.alerts:
             k = alert[0]
@@ -197,7 +193,7 @@ Lots of email is annoying so it can only send every 5 minutes.
         """
         today = datetime.datetime.now()
         message = (msg + " in " + str(self.unit).lower() +
-                   " @ " + today.strftime('%b %d %Y %H:%M')) 
+                   " @ " + today.strftime('%b %d %Y %H:%M'))
         # Tweet
         # The reason I had to pull in the thingspeak handling class,
         # is that I'm using the thingspeak API to tweet.
